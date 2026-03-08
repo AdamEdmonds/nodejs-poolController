b385255fe866c755acf95fcda662988d995d50b6
AdamEdmonds <adam.edmonds@gmail.com>
2026-03-08 10:06:07 -0400

    Udpate host name.

--- scripts/buildandsend.ps1
+++ scripts/buildandsend.ps1
@@ -1,5 +1,8 @@
+#Ensure SSH is configured on Pi
+# sudo raspi-config -> Interface -> SSH -> enable.
+
 Remove-Item -Recurse -Force .\dist, .\node_modules
 npm ci
 npm run build
 Compress-Archive -Path .\dist, .\config, .\defaultConfig.json, .\package.json, .\package-lock.json -DestinationPath .\poolController-runtime.zip -Force
-scp .\poolController-runtime.zip edmonds@poolcon.local:~\
+scp .\poolController-runtime.zip pool@192.168.4.41:~\
